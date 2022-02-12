1dd4d99e2c0854345a12916c159a2a51a6528705
AtomicRogue1 <30554253+AtomicRogue1@users.noreply.github.com>
2022-02-12 21:55:32 +0530

    Basic Main Menu added.
    My mind goes to more stuff for the game but first, we need to make all levels

---
+++ Assets/UIStuff/ToGame.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ToGame : MonoBehaviour
+{
+    // [SerializeField]
+    // Image LoadingScreen;
+    public void Play()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+    // public void LoadingLevel()
+    // {
+    //     Invoke("Play",5f);
+    //     float Velocity=0;
+    //     float smoothTime=0.1f;
+    //     float col;
+    //     while(LoadingScreen.color.a!=1)
+    //     {
+    //         col=Mathf.SmoothDamp(LoadingScreen.color.a, 1, ref Velocity, smoothTime);
+    //         LoadingScreen.color.a=col;
+    //     }
+    // }
+}
